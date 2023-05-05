8a5eccde7a0685a19697bb81c1761be5884da0ed
abiodunraheem <raheemabiodun23@gmail.com>
2023-05-05 12:01:01 +0100

    Add authentication to project

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,10 @@
 import { useState, useEffect } from 'react'
 import { FaSignInAlt } from 'react-icons/fa'
+import {useSelector, useDispatch} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+import {toast} from 'react-toastify'
+import {login, reset} from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 
 function Login() {
@@ -7,6 +12,28 @@ function Login() {
     email: '',
     password: '',
   })
+
+  const {email, password} = formData
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const {user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
+
+  useEffect(() => {
+    if(isError) {
+      toast.message(message)
+    }
+
+    if(isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+
+  }, [user, isError, isSuccess, navigate,dispatch])
 
   const onChange = (e) => {
     setFormData((previousState) => ({
@@ -17,8 +44,19 @@ function Login() {
 
   const onSubmit = (e) => {
     e.preventDefault()
+
+    const userData = {
+      email,
+      password
+    }
+
+    dispatch(login(userData))
   }
-  const {email, password} = formData
+
+  if(isLoading) {
+    return <Spinner />
+  }
+
   return <>
   <section className="heading">
     <h1>
--- frontend/src/pages/Resgister.jsx
+++ frontend/src/pages/Resgister.jsx
@@ -1,5 +1,10 @@
 import { useState, useEffect } from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+import {toast} from 'react-toastify'
 import { FaUser } from 'react-icons/fa'
+import {register, reset} from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 
 function Resgister() {
@@ -10,6 +15,28 @@ function Resgister() {
     password2: ''
   })
 
+  const {name, email, password,password2} = formData
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const {user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
+
+  useEffect(() => {
+    if(isError) {
+      toast.message(message)
+    }
+
+    if(isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+
+  }, [user, isError, isSuccess, navigate,dispatch])
+
   const onChange = (e) => {
     setFormData((previousState) => ({
       ...previousState,
@@ -19,8 +46,24 @@ function Resgister() {
 
   const onSubmit = (e) => {
     e.preventDefault()
+
+    if(password !== password2) {
+      toast.error('passwordsdo not match')
+    } else {
+      const userData = {
+        name,
+        email,
+        password,
+      }
+
+      dispatch(register(userData))
+    }
   }
-  const {name, email, password,password2} = formData
+
+  if(isLoading) {
+    return <Spinner />
+  }
+
   return <>
   <section className="heading">
     <h1>
